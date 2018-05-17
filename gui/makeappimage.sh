453082adbda099dff03b9b83859b151544990f9f
tsteven4 <tsteven4@gmail.com>
2018-05-17 16:48:18 -0600

    use recommended workaround for libsoftokn.

--- gui/makeappimage.sh
+++ gui/makeappimage.sh
@@ -5,20 +5,22 @@ QMAKE=${QMAKE:-qmake}
 LINUXDEPLOYQT=${LINUXDEPLOYQT:-linuxdeployqt}
 rm -fr $DISTDIR
 mkdir $DISTDIR
-cp gpsbabelfe.desktop $DISTDIR
-cp images/appicon.png $DISTDIR/gpsbabelfe.png
-cp objects/gpsbabelfe-bin $DISTDIR/gpsbabelfe
+cp -p gpsbabelfe.desktop $DISTDIR
+cp -p images/appicon.png $DISTDIR/gpsbabelfe.png
+cp -p objects/gpsbabelfe-bin $DISTDIR/gpsbabelfe
 if [ -x ../gpsbabel ]; then
-  cp ../gpsbabel $DISTDIR/gpsbabel
+  cp -p ../gpsbabel $DISTDIR/gpsbabel
 elif [ -x ../GPSBabel ]; then
-  cp ../GPSBabel $DISTDIR/gpsbabel
+  cp -p ../GPSBabel $DISTDIR/gpsbabel
 else
   echo "Couldn't find command line executable gpsbabel or GPSBabel." >&2
   exit 1;
 fi
-cp COPYING.txt $DISTDIR
-cp gmapbase.html $DISTDIR
+cp -p COPYING.txt $DISTDIR
+cp -p gmapbase.html $DISTDIR
 mkdir $DISTDIR/translations
-cp *.qm $DISTDIR/translations
-# https://github.com/probonopd/linuxdeployqt
-$LINUXDEPLOYQT $DISTDIR/gpsbabelfe -qmake=$QMAKE -appimage -verbose=2 -executable=$DISTDIR/gpsbabel -exclude-libs="libnss3.so,libnssutil3.so" 2>&1 | tee makeappimage.log
+cp -p *.qm $DISTDIR/translations
+#https://github.com/probonopd/linuxdeployqt/issues/35
+mkdir $DISTDIR/lib
+cp -pr /usr/lib/x86_64-linux-gnu/nss $DISTDIR/lib/
+$LINUXDEPLOYQT $DISTDIR/gpsbabelfe -qmake=$QMAKE -appimage -verbose=2 -executable=$DISTDIR/gpsbabel  2>&1 | tee makeappimage.log
