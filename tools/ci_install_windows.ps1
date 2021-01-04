e30112e76b78c18cc5c8f47ca7f275849721e7d4
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-03 19:15:51 -0700

    try ps again

---
+++ tools/ci_install_windows.ps1
@@ -0,0 +1,10 @@
+Set-PSDebug -Trace 1
+& "$Env:windir\System32\curl.exe" -s -L -o qt-opensource-windows-x86-5.12.10.exe "https://download.qt.io/archive/qt/5.12/5.12.10/qt-opensource-windows-x86-5.12.10.exe"
+netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="$Env:Pwd\qt-opensource-windows-x86-5.12.10.exe"
+& "$Env:Pwd\qt-opensource-windows-x86-5.12.10.exe" --verbose --script "$Env:CI_BUILD_DIR\tools\qtci\qt-install.qs" QTCI_OUTPUT="$Env:Home\Cache\Qt" QTCI_PACKAGES=qt.qt5.51210.win64_msvc2017_64,qt.qt5.51210.qtwebengine.win64_msvc2017_64
+netsh advfirewall firewall delete rule name=dummyupqt
+dir "$Env:Home"
+dir "$Env:Home\Cache"
+dir "$Env:Home\Cache\Qt"
+& "$Env:Home\Cache\Qt\5.12.10\msvc2017_64\bin\qmake.exe" -v
+
