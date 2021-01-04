be594c69f963e13b62e1d6d192ef02b1c8465494
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-03 17:52:20 -0700

    and again

--- tools/ci_install_windows.ps1
+++ tools/ci_install_windows.ps1
@@ -1,8 +1,9 @@
-Set-PSDebug -Trace 1
+Set-PSDebug -Trace 2
 C:\Windows\System32\curl -s -L -o qt-opensource-windows-x86-5.12.10.exe "https://download.qt.io/archive/qt/5.12/5.12.10/qt-opensource-windows-x86-5.12.10.exe"
 netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="$Pwd\qt-opensource-windows-x86-5.12.10.exe"
-& "$Pwd\qt-opensource-windows-x86-5.12.10.exe" --verbose --platform minimal --script "tools\qtci\qt-install.qs" QTCI_OUTPUT="$Home\Cache\Qt" QTCI_PACKAGES=qt.qt5.51210.win64_msvc2017_64,qt.qt5.51210.qtwebengine.win64_msvc2017_64
+& "$Pwd\qt-opensource-windows-x86-5.12.10.exe" --verbose --platform minimal --script "$Env:CI_BUILD_DIR\tools\qtci\qt-install.qs" QTCI_OUTPUT="$Home\Cache\Qt" QTCI_PACKAGES=qt.qt5.51210.win64_msvc2017_64,qt.qt5.51210.qtwebengine.win64_msvc2017_64
 netsh advfirewall firewall delete rule name=dummyupqt
+Get-ChildItem "$Env:CI_BUILD_DIR"
 Get-ChildItem "$Home"
 Get-ChildItem "$Home\Cache"
 Get-ChildItem "$Home\Cache\Qt"
