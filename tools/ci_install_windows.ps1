38f4cf9e6d93f5fb312bc73c681f3bf117ac0a2f
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-03 17:09:48 -0700

    windows againg

--- tools/ci_install_windows.ps1
+++ tools/ci_install_windows.ps1
@@ -2,5 +2,5 @@ C:\Windows\System32\curl -L -o qt-opensource-windows-x86-5.12.10.exe "https://do
 netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="$Pwd\qt-opensource-windows-x86-5.12.10.exe"
 & "$Pwd\qt-opensource-windows-x86-5.12.10.exe" --verbose --platform minimal --script qt-install.qs QTCI_OUTPUT="$Home\Cache\Qt" QTCI_PACKAGES=qt.qt5.51210.win64_msvc2017_64,qt.qt5.51210.qtwebengine.win64_msvc2017_64
 netsh advfirewall firewall delete rule name=dummyupqt
-& "$Home\Cache\Qt\5.12.10\msvc2017_64\bin\qmake" -v
+& "$Home\Cache\Qt\5.12.10\msvc2017_64\bin\qmake.exe" -v
 
