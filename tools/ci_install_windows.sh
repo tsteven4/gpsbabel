8bad6da5726f27216c5df6ca8ccb17f72a7a1470
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-04 08:07:36 -0700

    parameterize windows

--- tools/ci_install_windows.sh
+++ tools/ci_install_windows.sh
@@ -1,23 +1,48 @@
 /bin/bash
 set -ex
 
+# validate install
+function validate() {
+  (
+    set +e
+    # shellcheck source=/dev/null
+    source "${CACHEDIR}/qt.env"
+    if [ "$(qmake -query QT_INSTALL_BINS)" != "${QTDIR}/bin" ]; then
+      echo "ERROR: unexpected Qt location." >&2
+      exit 1
+    fi
+    if [ "$(qmake -query QT_VERSION)" != "${QT_VERSION}" ]; then
+      echo "ERROR: wrong Qt version." >&2
+      exit 1
+    fi
+  )
+}
+
 QT_VERSION=${1:-5.12.10}
+ARCH=${2:-x86_amd64}
+if [ "$ARCH" = "x86_am64" ]; then
+  PACKAGE_SUFFIX=win64_msvc2017_64
+  COMPILER=msvc2017_64
+else
+  PACKAGE_SUFFIX=win32_msvc2017
+  COMPILER=msvc2017
+fi
 
 CACHEDIR=${HOME}/Cache
-QTDIR=${CACHEDIR}/Qt/${QT_VERSION}/msvc2017_64
+QTDIR=${CACHEDIR}/Qt/${QT_VERSION}/${COMPILER}
 
-if [ -d "${QTDIR}/binxx" ]; then
+if [ -d "${QTDIR}/bin" ]; then
   echo "Using cached Qt."
 else
   rm -fr ${CACHEDIR}
   QT_VERSION_SHORT=${QT_VERSION//./}
   curl -s -L -o qt-opensource-windows-x86-${QT_VERSION}.exe "https://download.qt.io/official_releases/qt/5.12/${QT_VERSION}/qt-opensource-windows-x86-${QT_VERSION}.exe"
   ls -l *.exe
-  #netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe"
   netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program=$(cygpath -w "${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe")
-  "${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe" --verbose --script "${CI_BUILD_DIR}/tools/qtci/qt-install.qs" QTCI_OUTPUT="${CACHEDIR}/Qt" QTCI_PACKAGES=qt.qt5.${QT_VERSION_SHORT}.win64_msvc2017_64,qt.qt5.${QT_VERSION_SHORT}.qtwebengine.win64_msvc2017_64
+  "${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe" --verbose --script "${CI_BUILD_DIR}/tools/qtci/qt-install.qs" QTCI_OUTPUT="${CACHEDIR}/Qt" QTCI_PACKAGES=qt.qt5.${QT_VERSION_SHORT}.${PACKAGE_SUFFIX},qt.qt5.${QT_VERSION_SHORT}.qtwebengine.${PACKAGE_SUFFIX}
   netsh advfirewall firewall delete rule name=dummyupqt
   rm qt-opensource-windows-x86-${QT_VERSION}.exe
+  echo "export PATH=${QTDIR}/bin:\$PATH" > ${CACHEDIR}/qt.env
 fi
-"${QTDIR}/bin/qmake.exe" -v
+validate
 
