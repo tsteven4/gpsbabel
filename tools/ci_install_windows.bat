fd8a7a557bec1f2805513aa40c691b0bcff8d0ca
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-03 18:18:29 -0700

    echo

--- tools/ci_install_windows.bat
+++ tools/ci_install_windows.bat
@@ -1,3 +1,4 @@
+echo on
 curl -s -L -o qt-opensource-windows-x86-5.12.10.exe "https://download.qt.io/archive/qt/5.12/5.12.10/qt-opensource-windows-x86-5.12.10.exe"
 netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="%cd%\qt-opensource-windows-x86-5.12.10.exe"
 "%cd%\qt-opensource-windows-x86-5.12.10.exe" --verbose --platform minimal --script "%CI_BUILD_DIR%\tools\qtci\qt-install.qs" QTCI_OUTPUT="%HOMEPATH%\Cache\Qt" QTCI_PACKAGES=qt.qt5.51210.win64_msvc2017_64,qt.qt5.51210.qtwebengine.win64_msvc2017_64
