5ff1fa8e2ac26886e14aff8b4ab852bdaa66daa3
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-03 19:46:46 -0700

    batch fun

--- tools/ci_install_windows.bat
+++ tools/ci_install_windows.bat
@@ -1,10 +1,21 @@
 echo on
-curl -s -L -o qt-opensource-windows-x86-5.12.10.exe "https://download.qt.io/archive/qt/5.12/5.12.10/qt-opensource-windows-x86-5.12.10.exe"
-netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="%cd%\qt-opensource-windows-x86-5.12.10.exe"
-"%cd%\qt-opensource-windows-x86-5.12.10.exe" --verbose --script "%CI_BUILD_DIR%\tools\qtci\qt-install.qs" QTCI_OUTPUT="%HOMEDRIVE%%HOMEPATH%\Cache\Qt" QTCI_PACKAGES=qt.qt5.51210.win64_msvc2017_64,qt.qt5.51210.qtwebengine.win64_msvc2017_64
-netsh advfirewall firewall delete rule name=dummyupqt
-dir "%HOMEDRIVE%%HOMEPATH%"
-dir "%HOMEDRIVE%%HOMEPATH%\Cache"
-dir "%HOMEDRIVE%%HOMEPATH%\Cache\Qt"
-"%HOMEDRIVE%%HOMEPATH%\Cache\Qt\5.12.10\msvc2017_64\bin\qmake.exe" -v
+SETLOCAL
+SET CACHEDIR="%HOMEDRIVE%%HOMEPATH%\Cache"
+SET QTDIR="%CACHEDIR%\Qt\%QT_VERSION%\msvc2017_64"
+IF EXIST "%CACHEDIR%\bin" (
+  echo "Using cached Qt."
+) ELSE (
+  DEL /S "%CACHEDIR%"
+  SET QT_VERSION_SHORT=%QT_VERSION:.=%
+  curl -s -L -o qt-opensource-windows-x86-%QT_VERSION%.exe "https://download.qt.io/archive/qt/5.12/%QT_VERSION%/qt-opensource-windows-x86-%QT_VERSION%.exe"
+  netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="%cd%\qt-opensource-windows-x86-%QT_VERSION%.exe"
+  "%cd%\qt-opensource-windows-x86-%QT_VERSION%.exe" --verbose --script "%CI_BUILD_DIR%\tools\qtci\qt-install.qs" QTCI_OUTPUT="%CACHEDIR%\Qt" QTCI_PACKAGES=qt.qt5.%QT_VERSION_SHORT%.win64_msvc2017_64,qt.qt5.%QT_VERSION_SHORT%.qtwebengine.win64_msvc2017_64
+  netsh advfirewall firewall delete rule name=dummyupqt
+  DEL qt-opensource-windows-x86-%QT_VERSION%.exe
+  DIR "%HOMEDRIVE%%HOMEPATH%"
+  DIR "%CACHEDIR%
+  DIR "%QTDIR%
+  "%CACHEDIR%\bin\qmake.exe" -v
+)
+ENDLOCAL
 
