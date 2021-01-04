663cf6ff2d4308ab3d0a30344bf761aa84270827
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-04 06:24:11 -0700

    ?

--- tools/ci_install_windows.sh
+++ tools/ci_install_windows.sh
@@ -12,7 +12,9 @@ else
   rm -fr ${CACHEDIR}
   QT_VERSION_SHORT=${QT_VERSION//./}
   curl -s -L -o qt-opensource-windows-x86-${QT_VERSION}.exe "https://download.qt.io/archive/qt/5.12/${QT_VERSION}/qt-opensource-windows-x86-${QT_VERSION}.exe"
-  netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe"
+  ls -l *.exe
+  #netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe"
+  netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program=$(cygpath -w "${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe")
   "${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe" --verbose --script "${CI_BUILD_DIR}/tools/qtci/qt-install.qs" QTCI_OUTPUT="${CACHEDIR}/Qt" QTCI_PACKAGES=qt.qt5.${QT_VERSION_SHORT}.win64_msvc2017_64,qt.qt5.${QT_VERSION_SHORT}.qtwebengine.win64_msvc2017_64
   netsh advfirewall firewall delete rule name=dummyupqt
   rm qt-opensource-windows-x86-${QT_VERSION}.exe
