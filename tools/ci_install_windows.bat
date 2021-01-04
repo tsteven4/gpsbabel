061f16ae7aa79501cc83ffd1b9a81b58a073e0ea
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-03 20:12:15 -0700

    really

--- tools/ci_install_windows.bat
+++ tools/ci_install_windows.bat
@@ -6,12 +6,9 @@ IF EXIST "%CACHEDIR%\bin" (
   echo "Using cached Qt."
 ) ELSE (
   DEL /S "%CACHEDIR%"
-  SET QT_VERSION_SHORT=%QT_VERSION:.=%
-  echo %QT_VERSION_SHORT%
-  echo QTCI_PACKAGES=qt.qt5.%QT_VERSION_SHORT%.win64_msvc2017_64,qt.qt5.%QT_VERSION_SHORT%.qtwebengine.win64_msvc2017_64
   curl -s -L -o qt-opensource-windows-x86-%QT_VERSION%.exe "https://download.qt.io/archive/qt/5.12/%QT_VERSION%/qt-opensource-windows-x86-%QT_VERSION%.exe"
   netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="%cd%\qt-opensource-windows-x86-%QT_VERSION%.exe"
-  "%cd%\qt-opensource-windows-x86-%QT_VERSION%.exe" --verbose --script "%CI_BUILD_DIR%\tools\qtci\qt-install.qs" QTCI_OUTPUT="%CACHEDIR%\Qt" QTCI_PACKAGES=qt.qt5.%QT_VERSION_SHORT%.win64_msvc2017_64,qt.qt5.%QT_VERSION_SHORT%.qtwebengine.win64_msvc2017_64
+  "%cd%\qt-opensource-windows-x86-%QT_VERSION%.exe" --verbose --script "%CI_BUILD_DIR%\tools\qtci\qt-install.qs" QTCI_OUTPUT="%CACHEDIR%\Qt" QTCI_PACKAGES=qt.qt5.%QT_VERSION:.=%.win64_msvc2017_64,qt.qt5.%QT_VERSION:.=%.qtwebengine.win64_msvc2017_64
   netsh advfirewall firewall delete rule name=dummyupqt
   DEL qt-opensource-windows-x86-%QT_VERSION%.exe
   DIR "%HOMEDRIVE%%HOMEPATH%"
