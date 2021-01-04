275318f90904c429a82ba958a91eaf564198f1b1
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-03 19:00:29 -0700

    hdhp

--- tools/ci_install_windows.bat
+++ tools/ci_install_windows.bat
@@ -3,9 +3,8 @@ curl -s -L -o qt-opensource-windows-x86-5.12.10.exe "https://download.qt.io/arch
 netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="%cd%\qt-opensource-windows-x86-5.12.10.exe"
 "%cd%\qt-opensource-windows-x86-5.12.10.exe" --verbose --script "%CI_BUILD_DIR%\tools\qtci\qt-install.qs" QTCI_OUTPUT="%HOMEDRIVE%%HOMEPATH%\Cache\Qt" QTCI_PACKAGES=qt.qt5.51210.win64_msvc2017_64,qt.qt5.51210.qtwebengine.win64_msvc2017_64
 netsh advfirewall firewall delete rule name=dummyupqt
-Get-ChildItem "%CI_BUILD_DIR%"
-Get-ChildItem "%HOMEPATH%"
-Get-ChildItem "%HOMEPATH%\Cache"
-Get-ChildItem "%HOMEPATH%\Cache\Qt"
-"%HOMEPATH%\Cache\Qt\5.12.10\msvc2017_64\bin\qmake.exe" -v
+dir "%HOMEDRIVE%%HOMEPATH%"
+dir "%HOMEDRIVE%%HOMEPATH%\Cache"
+dir "%HOMEDRIVE%%HOMEPATH%\Cache\Qt"
+"%HOMEDRIVE%%HOMEPATH%\Cache\Qt\5.12.10\msvc2017_64\bin\qmake.exe" -v
 
