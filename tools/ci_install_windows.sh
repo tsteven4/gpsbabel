c26b4b7bb10e79acc23c99dfe6da5c8d2b4425f9
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2021-01-30 11:42:42 -0700

    try aqt for Qt install on windows

--- tools/ci_install_windows.sh
+++ tools/ci_install_windows.sh
@@ -24,6 +24,10 @@ if [ "${COMPILER}" = "msvc2017_64" ]; then
   PACKAGE_SUFFIX=win64_msvc2017_64
 elif [ "${COMPILER}" = "msvc2017" ]; then
   PACKAGE_SUFFIX=win32_msvc2017
+elif [ "${COMPILER}" = "msvc2019_64" ]; then
+  PACKAGE_SUFFIX=win64_msvc2019_64
+elif [ "${COMPILER}" = "msvc2019" ]; then
+  PACKAGE_SUFFIX=win32_msvc2019
 else
   echo "ERROR: unrecognized Qt compiler ${COMPILER}." >&2
   exit 1
@@ -36,18 +40,8 @@ if [ -d "${QTDIR}/bin" ]; then
   echo "Using cached Qt."
 else
   rm -fr "${CACHEDIR}"
-  QT_VERSION_SHORT=${QT_VERSION//./}
-  curl -s -L -o "qt-opensource-windows-x86-${QT_VERSION}.exe" "https://download.qt.io/official_releases/qt/5.12/${QT_VERSION}/qt-opensource-windows-x86-${QT_VERSION}.exe"
-  ls -l ./*.exe
-  netsh advfirewall firewall add rule name=dummyupqt dir=out action=block program="$(cygpath -w "${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe")"
-  "${PWD}/qt-opensource-windows-x86-${QT_VERSION}.exe" --verbose --script "${CI_BUILD_DIR}/tools/qtci/qt-install.qs" QTCI_OUTPUT="${CACHEDIR}/Qt" QTCI_PACKAGES="qt.qt5.${QT_VERSION_SHORT}.${PACKAGE_SUFFIX},qt.qt5.${QT_VERSION_SHORT}.qtwebengine"
-  netsh advfirewall firewall delete rule name=dummyupqt
-  rm "qt-opensource-windows-x86-${QT_VERSION}.exe"
-  ls "${CACHEDIR}/Qt"
-  rm -fr "${CACHEDIR}/Qt/Docs"
-  rm -fr "${CACHEDIR}/Qt/Examples"
-  rm -fr "${CACHEDIR}/Qt/Tools"
-  rm -f "${CACHEDIR}/Qt/MaintenanceTool.*"
+  python -mpip aqtinstall
+  aqt install -O "${CACHEDIR}/Qt" ${QT_VERSION} windows desktop ${PACKAGE_SUFFIX} -m qtwebengine
   echo "export PATH=${QTDIR}/bin:\$PATH" > "${CACHEDIR}/qt.env"
 fi
 validate
