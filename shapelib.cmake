370e9a2a7515b84b46e45da766414522f1f1e0cf
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2023-06-30 09:21:28 -0600

    add build support for msvc clang-cl

--- shapelib.cmake
+++ shapelib.cmake
@@ -19,10 +19,17 @@ else()
     # note gpsbabel has conditional code include "shapelib/shapefil.h",
     # so it doesn't actually rely on the include directory being PUBLIC/INTERFACE
     target_include_directories(shp PUBLIC shape)
+    target_compile_definitions(shp PRIVATE DISABLE_CVSID)
     if(MSVC)
       target_compile_definitions(shp PRIVATE _CRT_SECURE_NO_WARNINGS)
       target_compile_definitions(shp PRIVATE _CRT_NONSTDC_NO_WARNINGS)
-      target_compile_options(shp PRIVATE /MP -wd4100 -wd4267)
+    endif()
+    # disable warning from shapelib, which we don't want to own or modify
+    if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+      target_compile_options(shp PRIVATE -wd4267)
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+      # MSVC frontend clang-cl default includes /W3 which is equivalent to -Wall.
+      target_compile_options(shp PRIVATE -Wno-unused-but-set-variable)
     endif()
     list(APPEND LIBS shp)
   elseif(GPSBABEL_WITH_SHAPELIB STREQUAL "custom")
