c0a3c4f2d1a152dd3e314d0985c51e71ca684e10
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2024-11-28 10:58:39 -0700

    install python build

--- tools/ci_install_aqt.sh
+++ tools/ci_install_aqt.sh
@@ -4,5 +4,6 @@ set -ex
 # This is a hack to get qtwebengine, qtpdf with 6.8.0.
 git clone https://github.com/tsteven4/aqtinstall
 cd aqtinstall
+python3 -m pipx install build
 python3 -m build
 python3 -m pipx install ./dist/aqtinstall-*-py3-none-any.whl
