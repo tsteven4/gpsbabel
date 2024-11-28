fb08fd509c838bc92431159705ff7246d6b89f00
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2024-11-28 11:05:43 -0700

    try some more

--- tools/ci_install_aqt.sh
+++ tools/ci_install_aqt.sh
@@ -4,6 +4,7 @@ set -ex
 # This is a hack to get qtwebengine, qtpdf with 6.8.0.
 git clone https://github.com/tsteven4/aqtinstall
 cd aqtinstall
+git checkout gpsbabel
 python3 -m pip install build
 python3 -m build
 python3 -m pip install ./dist/aqtinstall-*-py3-none-any.whl
