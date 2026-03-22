aa9f64f3a5459fbb9c9de0534e35f7fedd480c33
tsteven4 <13596209+tsteven4@users.noreply.github.com>
2026-03-21 20:43:18 -0600

    fix empty relative path, extra /

--- tools/make_gpsbabel_doc.sh
+++ tools/make_gpsbabel_doc.sh
@@ -4,7 +4,7 @@ set -ex
 sourcedir=$(cd "$(dirname "$0")/.." && pwd)
 
 perl "${sourcedir}/xmldoc/makedoc"
-xmllint --noout --relaxng "file:///${sourcedir}/tools/schema/docbook/xml/5.0/rng/docbook.rng" xmldoc/readme.xml
+xmllint --noout --relaxng "file://${sourcedir}/tools/schema/docbook/xml/5.0/rng/docbook.rng" xmldoc/readme.xml
 # the following doesn't seem to work.
 #xmllint --noout --schematron http://docbook.org/xml/5.0/sch/docbook.sch xmldoc/readme.xml
 # jing and many depedencies removed from fedora
